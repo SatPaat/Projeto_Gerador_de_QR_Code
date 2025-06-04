d98e3183391c4257ac3d24a543f4be7d6be0a1d5
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-06-03 21:06:50 -0300

    commit final

--- js/script.js
+++ js/script.js
@@ -1,2 +1,43 @@
-const hTeste = document.querySelector("#h1-teste")
-console.log(hTeste);
+//Selecionando
+const container = document.querySelector(".container")
+const qrCodeBtn = document.querySelector(".qr-form button")
+const qrCodeInput = document.querySelector(".qr-form input")
+const qrCodeImg = document.querySelector("#qr-code img")
+
+
+//Eventos
+
+//Gerar QR
+//  Code
+function generatorQrCode(){
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}}`
+
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add("active")
+        qrCodeBtn.innerText = "código criado!";
+    })
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generatorQrCode();
+})
+
+qrCodeInput.addEventListener("keydown", (e) => {
+    if(e.code === "Enter"){
+        generatorQrCode();
+    }
+})
+
+//Limpar QR Code
+qrCodeInput.addEventListener("keyup", (e) => {
+    if(!qrCodeInput.value){
+        container.classList.remove("active")
+        qrCodeBtn.innerText = "Gerar QR Code";
+    }
+})
